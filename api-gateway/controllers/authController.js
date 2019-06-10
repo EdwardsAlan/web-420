408b52a82fb0d6d615a95fea7b163631a2cd88b4
EdwardsAlan <alan.edwards1103@gmail.com>
2019-06-09 21:40:40 -0400

    Update authController.js

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -6,63 +6,62 @@
 ; Description: API GATEWAY
 ;===========================================
 */
-
-var User = require('../models/user');
-
-//Register a new user on POST
-
-exports.user_register = function(req, res) {
-    res.send('NOT IMPLEMENTED: User registration POST');
-};
-
-//Verify token on GET
-
-exports.user_token =function(req,res) {
-    res.send('NOT IMPLEMENTED; User token lookup GET');
-};
-
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
-
+var bodyParser = require('body-parser')
+// Register a new user on POST 
 exports.user_register = function(req, res) {
     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-    
+    console.log("Good!");
     var newUser = new User({
         username: req.body.username,
         password: hashedPassword,
         email: req.body.email
     });
-User.add(newUser, (err, user) => {
-    if(err)
-        return res.status(500).send ('There was a problem registering the user.');
-
-    var token = jwt.sign({ id: user._id}, config.web.secret, {
-        expiresIn: 86400 // 24 hours
-    });
-
-        res.status(200).send({ auth: true, token: token });
-    });
-};
-
-//Verify token on GET
-exports.user_token = function (req, res) {
-    var token = req.headers['x-access-token'];
-
-    if(!token) return res.status(401).send({ auth: false, message: 'No token provided'});
-
-    jwt.verify(token, config.web.secret, function(err, decoded){
-        if(err) return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
-
-        User.getById(decoded.id, function(err, user){
-            if (err) return res.status(500).send('There was a problem finding the user.');
-
-            if(!user) return res.status(404).send('No user found.');
-
-            res.status(200).send(user);
+    console.log("Object Created");
+    User.add(newUser, (err, user) =>{
+        if (err){
+            console.log("Error with registration!");
+            return res.status(500).send("There was a problem with registering the user.");
+        }
+        console.log("No error with registration!");
+        var token = jwt.sign({ id: user._id}, config.web.secret, {
+            expiresIn: 86400
         });
-    
-    });
+        res.status(200).send({auth: true, token: token});
+        console.log("Sent information on JWT!");
+    })
+}; 
+// Verify token on GET 
+exports.user_token = function(req, res) {
+    User.getById(req.userId, function(err, user){
+        if(err) return res.status(500).send('There was a problem finding the user.');
+        if(!user) return res.status(404).send('No user found.');
 
+        res.status(200).send(user);
+    });
 };
+
+exports.user_login = function(req, res){
+    console.log(req.body.email);
+    User.getOne(req.body.email, function(err, user){
+        if (err) return res.status(500).send('Error on Server');
+        if (!user) return res.status(404).send('User not found');
+        
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+        
+        if(!passwordIsValid) return res.status(401).send({ auth: false, token: null});
+
+        var token = jwt.sign({id: user._id}, config.web.secret, {
+            expiresIn: 86400
+        });
+
+        res.status(200).send( {auth: true, token: token});
+    })
+}
+
+exports.user_logout = function(req, res){
+    res.status(200).send({auth: false, token: null});
+}
