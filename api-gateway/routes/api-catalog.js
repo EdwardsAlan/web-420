2123be4ce8159f8ed5eacdc5d55e5c2e398e89a3
EdwardsAlan <alan.edwards1103@gmail.com>
2019-06-23 23:22:25 -0400

    week 8

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -7,15 +7,17 @@
 ;===========================================
 */
 
-var express = require('express');
-var router = express.Router();
+var express = require('express'); var router = express.Router();
 var auth_controller = require('../controllers/authController');
-
-//POST request for registering a user
-router.post('/auth/register', auth_controller.user_register);
-
-//GET request for verifying ujser tokens
-router.get('/auth/token',auth_controller.user_token);
-
-module.exports =router;
-
+var bodyParser = require('body-parser')
+var checkToken = require("../check-token");
+// POST request for registering a user 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+router.post('/auth/register', urlencodedParser, auth_controller.user_register);
+// GET request for verifying user tokens 
+router.get('/auth/token', checkToken, auth_controller.user_token);
+// POST request for logging a user in
+router.post('/auth/login', urlencodedParser, auth_controller.user_login);
+// GET request for logging a user out
+router.get('/auth/logout', auth_controller.user_logout);
+module.exports = router;
